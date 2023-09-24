7effb529d9122bf9e710d445ad287e4d57aa9420
Faizal Khan <fk624178@gmail.com>
2023-09-24 19:55:54 +0530

    upgrading the whole fucking project

---
+++ Assets/The Great Fleece/Game/Scripts/CameraTrigger.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraTrigger : MonoBehaviour {
+
+	[SerializeField] private GameObject _player;
+
+	private void OnTriggerEnter(Collider other)
+	{
+		if(other.CompareTag("Player"))
+		{
+			Debug.Log("Trigger was triggered");
+		}
+	}
+}
