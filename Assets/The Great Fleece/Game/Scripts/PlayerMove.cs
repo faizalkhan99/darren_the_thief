7e59a2581f2bc8bebd034d9bcb4d543b38d24cb9
Faizal Khan <fk624178@gmail.com>
2023-09-24 16:43:06 +0530

    added player movement

---
+++ Assets/The Great Fleece/Game/Scripts/PlayerMove.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.Experimental.UIElements;
+
+public class PlayerMove : MonoBehaviour {
+	private NavMeshAgent _agent;
+
+    void Start () 
+	{
+		_agent = GetComponent<NavMeshAgent>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Mouse0))
+		{
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			RaycastHit hit;
+			if(Physics.Raycast(ray, out hit))
+			{
+				_agent.destination = hit.point;
+			}
+		}
+	}
+}
