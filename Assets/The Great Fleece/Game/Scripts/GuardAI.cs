d965e0caf58984f3ff3081b40a18e628726a44e2
Faizal Khan <fk624178@gmail.com>
2023-09-25 19:30:26 +0530

    Guard's idle and walk Animations done

--- Assets/The Great Fleece/Game/Scripts/GuardAI.cs
+++ Assets/The Great Fleece/Game/Scripts/GuardAI.cs
@@ -6,25 +6,98 @@ using UnityEngine.AI;
 public class GuardAI : MonoBehaviour
 {
     [SerializeField] private List<Transform> wayPoints;
-    [SerializeField] private Transform currentTarget;
+    [SerializeField] private int _currentWaypoint;
     private NavMeshAgent _agent;
+    private bool _reversePath;
+    private bool _paused;
+    private bool _iswalking;
+    Animator _animator;
 
-    void Start()
+    void Awake()
     {
+        _animator = GetComponent<Animator>();
         _agent = GetComponent<NavMeshAgent>();
-        if(wayPoints.Count > 0)
-        {
-            if (wayPoints[0] != null) 
-            { 
-                currentTarget = wayPoints[0];
-                _agent.SetDestination(currentTarget.position);
-            }
-        }
+        if (_agent == null) Debug.Log("Agent was NULL");
     }
 
-    // Update is called once per frame
+
     void Update()
     {
-        
+        CalculateMovement();
+    }
+
+    void CalculateMovement()
+    {
+        if ( ( wayPoints.Count > 0) && (wayPoints[_currentWaypoint] != null) )
+        {
+            _agent.destination = wayPoints[_currentWaypoint].position;
+            float dist = Vector3.Distance(transform.position, wayPoints[_currentWaypoint].position);
+            
+            if (dist < 1 && (_currentWaypoint == 0 || _currentWaypoint == wayPoints.Count - 1)) _animator.SetBool("walk", false);
+            else _animator.SetBool("walk", true);
+            
+            if (dist < 1.0f && !_paused)
+            {
+                if ( (_currentWaypoint == 0) || (_currentWaypoint == wayPoints.Count - 1) )
+                {
+                    _paused = true;
+                    StartCoroutine(PauseGuard());
+                }
+                else
+                {
+                    if (_reversePath)
+                    {
+                        _currentWaypoint--;
+                        if (_currentWaypoint <= 0)
+                        {
+                            _reversePath = false;
+                            _currentWaypoint = 0;
+                        }
+                    }
+                    else
+                    {
+                        _currentWaypoint++;
+                    }
+                }
+            }
+        }
+        else Debug.Log("NULL");
+       
+    }
+
+    IEnumerator PauseGuard()
+    {
+        if (_currentWaypoint == 0)
+        {
+            Debug.Log("Start");
+            yield return new WaitForSeconds(2.0f);
+        }
+        else if (_currentWaypoint == wayPoints.Count - 1)
+        {
+            Debug.Log("End");
+            yield return new WaitForSeconds(2.0f);
+        }
+        else yield return null;
+
+     
+            if (_reversePath)
+            {
+                _currentWaypoint--;
+                if(_currentWaypoint == 0)
+                {
+                    _reversePath = false;
+                    _currentWaypoint = 0;
+                }
+            }
+            else if(!_reversePath)
+            {
+                _currentWaypoint++;
+                if( _currentWaypoint == wayPoints.Count)
+                {
+                    _reversePath = true;
+                    _currentWaypoint--;
+                }
+            }
+        _paused = false;
     }
 }
