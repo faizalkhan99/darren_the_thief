567dae0a07797584847b3cc739427a66bef169c3
Faizal Khan <fk624178@gmail.com>
2023-09-28 20:05:17 +0530

    coin pickup sound added

--- Assets/The Great Fleece/Game/Scripts/CoinManager.cs
+++ Assets/The Great Fleece/Game/Scripts/CoinManager.cs
@@ -5,12 +5,13 @@ using UnityEngine;
 
 public class CoinManager : MonoBehaviour
 {
+    [SerializeField] private AudioClip _coinpickup;
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
             PlayerMove._hasCoin = true;
-            Debug.Log("Coin Collected");
+            AudioSource.PlayClipAtPoint(_coinpickup,Camera.main.transform.position);
             Destroy(gameObject, 0.5f);
         }
     }
